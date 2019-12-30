1a8d11643416b08be38ea2a6544660b48b4945bd
Mickael Alliel <mickael@rookout.com>
2019-12-30 11:47:01 +0000

    fix missing package

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,15 +9,7 @@ namespace TodoDemoDotNet
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                        "~/Scripts/jquery-1.4.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/vue").Include(
                      "~/Scripts/vue.js"));
