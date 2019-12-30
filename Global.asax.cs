b22d323d3e608ba6b9f1ddb559434ffbc101be9c
Mickael Alliel <mickael@rookout.com>
2019-12-30 10:48:12 +0000

    Revert "done"
    This reverts commit 3d3beed3fa2c1f97b4718b2add98700f8375a9fd.

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Rook;
 
 namespace TodoDemoDotNet
 {
@@ -16,35 +13,7 @@ namespace TodoDemoDotNet
     {
         protected void Application_Start()
         {
-            try
-            {
-                System.IO.Directory.CreateDirectory(@"C:\\temp");
-
-            }
-            catch { }
-            Console.SetOut(new System.IO.StreamWriter(@"C:\\temp\\ConsoleOutput.txt"));
-            Thread pingThread = new Thread(() =>
-            {
-                while (true)
-                {
-                    Console.Out.Flush();
-                    Thread.Sleep(1000);
-                }
-            });
-            pingThread.Start();
-
-            try
-            {
-                Rook.RookOptions o = new RookOptions();
-                o.token = "TOKEN";
-                o.debug = true;
-                Rook.API.Start(o);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            
+            Rook.API.Start();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
